021cf50c06a0de9c0ac6e8a9acc516960a697c1f
lllytnik <65924940+lllytnik@users.noreply.github.com>
2023-04-29 00:17:13 +0400

    add project

--- src/s2-homeworks/hw01/HW1.tsx
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -14,7 +14,21 @@ import avatar from './avatar.png'
 * */
 
 // нужно создать правильный тип вместо any
-export type MessageType = any
+export type MessageType = {
+    id: number
+    user: UserTypeObj
+    message: MessageTypeObj
+}
+type UserTypeObj = {
+    avatar: string
+    name: string
+}
+type MessageTypeObj = {
+    text: string
+    time: string
+
+
+}
 
 // структуру объекта не менять
 export const message0: MessageType = {
