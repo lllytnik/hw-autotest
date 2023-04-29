0a3fc0ac33a880be36778d612f17585cd1613a0f
lllytnik <papa.carlo.link@gmail.com>
2023-04-29 20:23:27 +0400

    alala

--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,18 @@ import './index.css'
 import App from './s1-main/App'
 import reportWebVitals from './reportWebVitals'
 import store from './s2-homeworks/hw10/bll/store'
-import {Provider} from 'react-redux'
+import { Provider } from 'react-redux'
+import { HashRouter } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
     <React.StrictMode>
-        {/*для дз 10*/}
-        <Provider store={store}>
-            <App/>
-        </Provider>
+        <HashRouter>
+            {/*для дз 10*/}
+            <Provider store={store}>
+                <App />
+            </Provider>
+        </HashRouter>
     </React.StrictMode>
 )
 
