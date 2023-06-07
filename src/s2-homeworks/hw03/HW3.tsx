d1d200d8cab2ea91f51f05a8c1c5e0a40018d461
lllytnik <papa.carlo.link@gmail.com>
2023-06-07 23:33:29 +0400

    add hw3

--- src/s2-homeworks/hw03/HW3.tsx
+++ src/s2-homeworks/hw03/HW3.tsx
@@ -19,20 +19,25 @@ import GreetingContainer from './GreetingContainer'
 
 // types
 export type UserType = {
-    _id: any // need to fix any
-    name: any // need to fix any
+    _id: string // need to fix any
+    name: string // need to fix any
 }
 
-export const pureAddUserCallback = (name: any, setUsers: any, users: any) => { // need to fix any
+export const pureAddUserCallback = (
+    name: string,
+    setUsers: React.Dispatch<React.SetStateAction<UserType[]>>,
+    users: UserType[]) => { // need to fix any
     const user = { // need to fix
+        _id: v1(),
+        name: name,
     }
     setUsers([...users, user])
 }
 
 const HW3 = () => {
-    const [users, setUsers] = useState<any>([]) // need to fix any
+    const [users, setUsers] = useState<UserType[]>([]) // need to fix any
 
-    const addUserCallback = (name: any) => { // need to fix any
+    const addUserCallback = (name: string) => { // need to fix any
         pureAddUserCallback(name, setUsers, users)
     }
 
@@ -42,9 +47,7 @@ const HW3 = () => {
             {/*для автоматической проверки дз (не менять)*/}
 
             <div className={s2.hw}>
-                <GreetingContainer
-                    users={users}
-                    addUserCallback={addUserCallback}
+                <GreetingContainer users={users} addUserCallback={addUserCallback}
                 />
             </div>
         </div>
