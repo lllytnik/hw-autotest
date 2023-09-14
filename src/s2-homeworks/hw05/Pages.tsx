41289574d37a598ad1201d8e29aacd96af196962
lllytnik <papa.carlo.link@gmail.com>
2023-09-15 01:11:57 +0400

    add hw5

---
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import Error404 from './pages/Error404'
+import PreJunior from './pages/PreJunior'
+import Junior from './pages/Junior'
+import JuniorPlus from './pages/JuniorPlus'
+
+export const PATH = {
+    PRE_JUNIOR: '/pre-junior',
+    JUNIOR: '/junior',
+    JUNIOR_PLUS: '/junior-plus',
+}
+
+function Pages() {
+    return (
+        <div>
+            <Routes>
+                {/* Redirect from '/' to '/pre-junior' */}
+                <Route path="/" element={<Navigate to={PATH.PRE_JUNIOR} />} />
+
+                {/* Route for /pre-junior */}
+                <Route path={PATH.PRE_JUNIOR} element={<PreJunior />} />
+
+                {/* Route for /junior */}
+                <Route path={PATH.JUNIOR} element={<Junior />} />
+
+                {/* Route for /junior-plus */}
+                <Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus />} />
+
+                {/* Catch-all route for non-existent pages */}
+                <Route path="*" element={<Error404 />} />
+            </Routes>
+        </div>
+    )
+}
+
+export default Pages
